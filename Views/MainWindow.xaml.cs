990c7488ae1259a2e6f462115fcfc59a7b031318
Whatareyoulaughingat <nikosnaoumv1@gmail.com>
2022-06-04 13:10:08 +0300

    No need for inheritance.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace SimpleModpackDownloader.Views;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : IViewFor<MainWindowViewModel>
+public partial class MainWindow
 {
     public MainWindow()
     {

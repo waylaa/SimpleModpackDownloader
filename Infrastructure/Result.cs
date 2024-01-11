1561065f1d0e3f56a3eb9cebe9f157321e6126ac
Wayla <nikosnaoum127@gmail.com>
2024-01-11 23:15:51 +0200

    added new type for better error handling

---
+++ Infrastructure/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SimpleModpackDownloader.Infrastructure;
+
+internal readonly record struct Result<T>(T? Value, Exception? Error)
+{
+    [MemberNotNullWhen(true, nameof(Value))]
+    internal bool IsSucessful => this != default && Value is not null && Error is null;
+
+    public static implicit operator Result<T>(T value) => new(value, null);
+
+    public static implicit operator Result<T>(Exception error) => new(default, error);
+}
